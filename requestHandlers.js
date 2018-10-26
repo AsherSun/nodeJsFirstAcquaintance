a5b3143d8824fc5675f467b20868976ab1ea9952
孙鑫磊 <sunxl@cnjiang.com>
2018-10-26 16:34:03 +0800

    test 阻塞与非阻塞

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,21 @@
 const exec = require('child_process').exec;
 
 function start(response) {
-  // function sleep(milliSeconds) {
-  //   var startTime = new Date().getTime();
-  //   while(new Date().getTime() < startTime + milliSeconds);
-  // }
-  // sleep(10000)
-  var content = 'empty'
   exec('dir', (err, stdout) => {
-    console.log('stdout: ', typeof stdout)
-    content += stdout
+    function sleep(milliSeconds) {
+      var startTime = new Date().getTime();
+      while(new Date().getTime() < startTime + milliSeconds);
+      return 'okay'
+    }
+    sleep(10000)
+    response(stdout)
   })
-  return 'Request handler "start" was called.' + content
 }
 
 function upload(response) {
-  return 'Request handler "upload" was called.'
+  response('Request handler "upload" was called.')
 }
+
 module.exports = {
   start,
   upload
--- router.js
+++ router.js
@@ -2,8 +2,6 @@ module.exports = function(path, resHandle, response) {
 if (typeof resHandle[path] === 'function') {
     return resHandle[path](response)
   } else {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write('404 Not found' +  resHandle['/start']());
-    response.end();
+    response('404 Not found');
   }
 }
